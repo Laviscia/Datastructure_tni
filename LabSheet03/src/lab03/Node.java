7bc36631c64894ee4c72d915e5a7f0abe4b5bf1f
Word.EXE <ieq140949@gmail.com>
2026-07-14 17:23:06 +0700

    Add files via upload

---
+++ LabSheet03/src/lab03/Node.java
@@ -0,0 +1,18 @@
+package lab03;
+
+public class Node {
+
+	public Object data; //data in node
+	public Node next; //pointer
+	
+	public Node(Object data) {
+		this.data = data;
+		this.next = null;
+	}
+	public String getAddress() {
+		return""+Integer.toHexString(System.identityHashCode(this));
+	}
+	 public static String getAddress(Node node) {
+	      return (node == null) ? "null" : node.getAddress();
+	}
+}
